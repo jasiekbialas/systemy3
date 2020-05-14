cc5c955543667564398eb750f86d7a1f0ceca78f
Jasiek <jasiek.bialas@gmail.com>
2020-05-14 16:08:52 +0200

    add build script

---
+++ Documents/build.sh
@@ -0,0 +1,6 @@
+cd /usr/src/minix/kernel; make && make install
+cd /usr/src/minix/lib/libsys; make && make install
+cd /usr/src/minix/servers/sched; make && make install
+cd /usr/src/minix/servers/pm; make && make install
+cd /usr/src/lib/libc; make && make install
+cd /usr/src/releasetools; make do-hdboot
